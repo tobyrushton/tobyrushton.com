2a5fe12f539fba1903309c71f882926ca52e7b4e
tobyrushton <tobyjrushton@icloud.com>
2024-03-20 23:19:00 +0000

    styled page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Raleway Variable', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                bg: '#0c0c0c',
+                fg: '#121212',
+            },
+        },
     },
     plugins: [],
 }
