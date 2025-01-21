582f0600f002c7b9afcd41bcc78c510f0e1fcf8e
tobyrushton <tobyjrushton@icloud.com>
2025-01-21 15:30:29 +0000

    lint all files

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,13 +2,13 @@ import { glob } from 'astro/loaders'
 import { z, defineCollection } from 'astro:content'
 
 const blog = defineCollection({
-    loader: glob({ pattern: "**/[^_]*.md", base: "./src/posts" }),
+    loader: glob({ pattern: '**/[^_]*.md', base: './src/posts' }),
     schema: z.object({
         title: z.string(),
         date: z.string(),
         description: z.string(),
         slug: z.string(),
-    })
+    }),
 })
 
-export const collections = { blog}
+export const collections = { blog }
